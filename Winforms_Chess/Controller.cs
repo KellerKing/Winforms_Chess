a2e338cfe1dc4126754484b14cf653e5fa075b2a
Luca Keller <Andreasks56@gmail.com>
2021-09-04 19:32:06 +0200

    Added A Check if the King is under Attack, and doesent allow that kind of illegal moves

--- Winforms_Chess/Controller.cs
+++ Winforms_Chess/Controller.cs
@@ -40,10 +40,20 @@ namespace Winforms_Chess
 
     public void GameObjectClicked(Coords coords, bool isPice)
     {
+      var moveResultDTO = MoveController.MakeMove(isPice,
+        new List<Pice>(m_Board.Pices),
+        coords, 
+        m_CurrentPlayer,
+        new List<Coords>(m_PossibleFelder),
+        m_SelectedPice == null ? null : (Pice)m_SelectedPice.Clone());
 
-      var moveResultDTO = MoveController.MakeMove(isPice, new List<Pice>(m_Board.Pices), coords, m_CurrentPlayer, new List<Coords>(m_PossibleFelder), m_SelectedPice == null ? null : (Pice)m_SelectedPice.Clone());
-
-      if (!moveResultDTO.WasMoveLegal) return;
+      if (!moveResultDTO.WasMoveLegal)
+      {
+        m_PossibleFelder.Clear();
+        m_SelectedPice = null;
+        return;
+      } 
+        
 
       if (moveResultDTO.WasFullMove)
       {
@@ -55,33 +65,6 @@ namespace Winforms_Chess
       m_SelectedPice = moveResultDTO.SelectedPice;
       m_PossibleFelder = moveResultDTO.PossibleFelder;
       m_Board.Pices = moveResultDTO.BoardPosition;
-
-      
-
-
-
-      //if (isPice)
-      //{
-      //  var clickedPice = (Pice)m_Board.Pices.First(x => x.Coord.Equals(coords)).Clone();
-
-      //  if (clickedPice.Owner == m_CurrentPlayer)
-      //  {
-      //    UpdatePossibleFelderAndSelectedPice(clickedPice);
-      //    return;
-      //  }
-      //  else if (m_PossibleFelder.Any(x => x.Equals(m_SelectedPice.Coord)))
-      //  {
-      //    //Das Definitiv auslagern : Ist ein Spielzug: Spieler wird geschlagen
-      //    m_SelectedPice.Coord = clickedPice.Coord;
-      //    m_Board.Pices.Remove(clickedPice);
-      //  }
-      //}
-      //else if (m_SelectedPice != null && m_PossibleFelder.Any(x => x.Equals(coords)))
-      //{
-      //  //Ist ein Spielzug: Vorher wurde eine Figur gewählt und dann ein Feld
-      //  m_SelectedPice.Coord = coords;
-      //  m_SelectedPice = null;
-      //}
     }
   }
 
--- Winforms_Chess/Rulebook.cs
+++ Winforms_Chess/Rulebook.cs
@@ -1,63 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Winforms_Chess
 {
   public static class Rulebook
   {
 
-    public static List<Coords> GetLegalMovesForSelectedPice(List<Pice> board, Pice selectedPice)
+    public static bool IsLegalMove(List<Pice> board, Player currentPlayer)
     {
-      return null;
+      return !IsKingInCheck(board, currentPlayer);
     }
 
-    private static bool IsKingInCheck(Player player, List<Pice> pices)
+    private static bool IsKingInCheck(List<Pice> board, Player currentPlayer)
     {
-      var king = pices.First(x => x.Owner == player && x.PiceType == PiceType.KING);
-      var enemy = player == Player.BLACK ? Player.WHITE : Player.BLACK;
+      var currentKing = board.FirstOrDefault(x => x.Owner == currentPlayer && x.PiceType == PiceType.KING);
 
-      return IsRookAttacking(enemy, pices, king) || IsPawnAttacking(enemy, pices, king) || 
-        IsKnigtAttacking(enemy, pices, king) || IsBishopAttacking(enemy, pices, king) || 
-        IsQueenAttacking(enemy, pices, king) || IsKingAttacking(enemy, pices, king);
-    }
-    //Rework mit Moves Klasse
-    private static bool IsRookAttacking(Player enemy, List<Pice> pices, Pice king)
-    {
-      return pices.Any(x => x.Coord.File == king.Coord.File && x.Owner == enemy) || pices.Any(x => x.Coord.Rank == king.Coord.Rank && x.Owner == enemy);
+      return IsPiceAttacking(board, currentKing, PiceType.ROOK) || IsPiceAttacking(board, currentKing, PiceType.KNIGHT) ||
+        IsPiceAttacking(board, currentKing, PiceType.BISHOP) || IsPiceAttacking(board, currentKing, PiceType.QUEEN) ||
+        IsPiceAttacking(board, currentKing, PiceType.KING) || IsPiceAttacking(board, currentKing, PiceType.PAWN);
     }
 
-    private static bool IsPawnAttacking(Player enemy, List<Pice> pices, Pice king)
+    private static bool IsPiceAttacking(List<Pice> board, Pice king, PiceType piceToCheck)
     {
-      var pawns = pices.Where(x => x.Owner == enemy && x.PiceType == PiceType.PAWN);
-      return pawns.Any(pawn => Move.GetMovesFor(pawn, pices).Contains(king.Coord));
-    }
+      var enemyPices = board.Where(x => x.Owner != king.Owner && x.PiceType == piceToCheck).ToList();
+      var possibleFelder = new List<Coords>();
+      enemyPices.ForEach(x => possibleFelder.AddRange(Move.GetMovesFor(x, board)));
+      return possibleFelder.Any(x => x.Equals(king.Coord));
 
-    private static bool IsKnigtAttacking(Player enemy, List<Pice> pices, Pice king)
-    {
-      var knights = pices.Where(x => x.Owner == enemy && x.PiceType == PiceType.KNIGHT);
-      return knights.Any(knight => Move.GetMovesFor(knight, pices).Contains(king.Coord));
-    }
+      //return CheckCheck(board, enemyPices, king.Owner);
 
-
-    private static bool IsBishopAttacking(Player enemy, List<Pice> pices, Pice king)
-    {
-      var bishops = pices.Where(x => x.Owner == enemy && x.PiceType == PiceType.BISHOP);
-      return bishops.Any(bishop => Move.GetMovesFor(bishop, pices).Contains(king.Coord));
-    }
-
-    private static bool IsQueenAttacking(Player enemy, List<Pice> pices, Pice king)
-    {
-      var queen = pices.FirstOrDefault(x => x.Owner == enemy && x.PiceType == PiceType.QUEEN);
-      return Move.GetMovesFor(queen, pices).Contains(king.Coord);
-    }
-
-    private static bool IsKingAttacking(Player enemy, List<Pice> pices, Pice king)
-    {
-      var enemyKing = pices.FirstOrDefault(x => x.Owner == enemy && x.PiceType == PiceType.KING);
-      return Move.GetMovesFor(enemyKing, pices).Contains(king.Coord);
     }
   }
 }
